78eb28533396f8f3a2d56179710ea0583c0e55e4
hvlmnns <sayhello@hvlmnns.de>
2015-06-04 18:26:45 +0200

    init

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  // import global settings
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      buildUgly: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'prod/jquery.plug.min.js':   'dev/jquery.plug.js',
+          'prod/jquery.latest.min.js': 'dev/jquery.latest.js',
+          'page/jquery.plug.min.js':   'dev/jquery.plug.js',
+          'page/jquery.latest.min.js': 'dev/jquery.latest.js',
+        },
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'page/style.css': 'dev/styles.styl'
+        },
+      }
+    },
+    watch: {
+      gruntchange: {
+        files: ['Gruntfile.js'],
+        options: {reload: true }
+      },
+      js: {
+        files: ['dev/**/*.js'],
+        tasks: ['uglify'],
+        options: {reload: true }
+      },
+      styl: {
+        files: ['dev/**/*.styl'],
+        tasks: ['stylus'],
+        options: {reload: true }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
